debf01cca572ef50c6510c416766c319774409f1
FACorreiaa <fernandocorreia316@gmail.com>
2022-11-01 16:07:26 +0100

    base config of prisma with nest WIP

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
+import { UserLoginService } from './user-login/user-login.service';
+import { UserLoginController } from './user-login/user-login.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UserController, UserLoginController],
+  providers: [AppService, UserService, UserLoginService],
 })
 export class AppModule {}
